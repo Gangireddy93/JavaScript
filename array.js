5fff7b8334eaeca8d540e1b55999b8dc7c779ea6
demo <demo@gmail.com>
2026-08-12 14:17:21 +0530

    Basics arrays

---
+++ array.js
@@ -0,0 +1,66 @@
+// var names=["A","b","c","d","e","f"];
+// console.log("names----",names, names.length);
+
+// names.push("Apple")
+// console.log(" add one varable names---",names)
+
+// names.unshift("Mango")
+// console.log("name-----",names)
+
+// names.shift("")
+// console.log("shift names--",names)
+// names.pop();
+// console.log("pop names--",names ,names.length);
+// names.shift();
+// console.log("shift names--",names ,names.length);
+
+// names.unshift("mango");
+// console.log("unshift names--",names ,names.length);
+// names.push("banana");
+// console.log("push names--",names,names.length);
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[-1]);
+
+// var fullNames="Gangireddy";
+// var str="reddy";
+// // // reverse=[....str].reverse().join('');
+// // console.log("length fullNames--",fullNames ,fullNames.length);
+
+// var names=["A","b","c","d","e","f"];
+// names[3]="R";
+// // console.log("names----",names[3], names.length);
+// console.log("names----",names, names.length)
+
+// for(var i=0; i<names.length;i++){
+//     console.log(names[i])
+// }
+
+// for(var i=str.length-1; i<str ;i++){
+//     console.log(str[i])
+// }
+
+// for(var i=str.length; i>=0;i--){
+//     console.log(str[i])
+
+// }
+// var va="REDDY"
+// var reverse="";
+// for(var i=va.length-1;i>=0 ;i--){
+//     // console.log(names[i]);
+//     reverse=reverse+va[i]
+// }
+//   console.log(reverse);
+
+// var reverse=[];
+// for(var i=names.length-1;i>=0 ;i--){
+//     // console.log(names[i]);
+//     reverse.push(names[i])
+// }
+//   console.log(reverse);
+
+for(i=0;i<10;i++){
+console.log(i)
+}
