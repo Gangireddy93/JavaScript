5fb095f23e4afad45ec11fe191267eab21659023
demo <demo@gmail.com>
2026-08-16 10:33:41 +0530

    Basic object Methods

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@
     "full name":"Gangi Reddy"
 };
 
-  console.log(Object.keys(details))
+  // console.log(Object.keys(details))
 
 // console.log(details);
 // console.log(details.full)
@@ -17,40 +17,40 @@
 // console.log(details.id)
 // console.log(details["full name"])
 
-// console.log(details["full name"]);
+console.log(details["full name"]);
 
  details["location"] = "Banglore"
 details.education = "B.tech";
 
-// delete details.id;
+delete details.id;
 
-//  console.log(details);
+ console.log(details);
 
-// var keys = Object.keys(details);
+var keys = Object.keys(details);
 
-//  console.log(keys)
-//  for(var i=0; i<= keys.length; i++){
+ console.log(keys)
+ for(var i=0; i<= keys.length; i++){
 
-    //  console.log( details["location"],i)
+     console.log( details["location"],i)
     
-    //  console.log(details[keys[i]],i)
+     console.log(details[keys[i]],i)
 
-//  }
-// var values= Object.values(details);
-// console.log(values)
-// for(var i=0; i<values.length; i++){
+ }
+var values= Object.values(details);
+console.log(values)
+for(var i=0; i<values.length; i++){
 
-//      console.log( details[values[i]],i)
-// }
+     console.log( details[values[i]],i)
+}
 
 
-// var details = {
-//     name: "Reddy",
-//     id: 35,
-//     age: 23,
-//     location: "KDR",
-//     "full name": "Gangi Reddy"
-// };
+var details = {
+    name: "Reddy",
+    id: 35,
+    age: 23,
+    location: "KDR",
+    "full name": "Gangi Reddy"
+};
 
 var values = Object.values(details);
 
