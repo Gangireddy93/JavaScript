dfb78c012f882ea7d8dfaf6b82de98600aec90ed
demo <demo@gmail.com>
2026-08-16 10:28:05 +0530

    Basic object Methods

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@
     "full name":"Gangi Reddy"
 };
 
-//  console.log(Object.keys(details))
+  console.log(Object.keys(details))
 
 // console.log(details);
 // console.log(details.full)
@@ -26,13 +26,34 @@ details.education = "B.tech";
 
 //  console.log(details);
 
-var keys = Object.keys(details);
+// var keys = Object.keys(details);
 
 //  console.log(keys)
- for(var i=0; i<= keys.length; i++){
+//  for(var i=0; i<= keys.length; i++){
 
-     console.log( details["location"],i)
+    //  console.log( details["location"],i)
     
-    //  console.log(details[keys[i]])
+    //  console.log(details[keys[i]],i)
 
- }
+//  }
+// var values= Object.values(details);
+// console.log(values)
+// for(var i=0; i<values.length; i++){
+
+//      console.log( details[values[i]],i)
+// }
+
+
+// var details = {
+//     name: "Reddy",
+//     id: 35,
+//     age: 23,
+//     location: "KDR",
+//     "full name": "Gangi Reddy"
+// };
+
+var values = Object.values(details);
+
+for (var i = 0; i < values.length; i++) {
+    console.log(values[i],i);
+}
